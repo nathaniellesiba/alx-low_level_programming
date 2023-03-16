7d8a6e52fe4317d1a6b25792d0805e407a67e178
Nathaniel <nathaniellesiba@yahoo.com>
2023-03-16 15:40:42 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - Programming is like building a multilingual puzzle * Return: 0 
- */
+*main - Programming is like building a multilingual puzzle
+*Return:0
+*/
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle"); return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
