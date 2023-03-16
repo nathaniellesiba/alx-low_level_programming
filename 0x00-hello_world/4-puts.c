556afde97706607c223059ed6dd16e634dceb199
Tsutsa <Nathaniel.Tsutsa@standardbank.co.za>
2023-03-17 00:10:01 +0200

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle * Return: 0 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"); return (0);
+}
