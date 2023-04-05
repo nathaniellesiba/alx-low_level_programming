916e4be9cb48a351fd0daf6682876cfed9302d63
Nathaniel <nathaniellesiba@yahoo.com>
2023-04-05 10:30:38 -0700

    strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to printed
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
@@ -11,7 +12,7 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
-	else 
+	else
 	{
 		return (1 + _strlen_recursion(s + 1));
 	}
