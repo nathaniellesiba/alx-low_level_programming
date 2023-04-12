83ddcc174fc8a953ca273465fdf57bc8cbc5619e
Nathaniel <nathaniellesiba@yahoo.com>
2023-04-11 18:11:30 -0700

    0x0B-malloc_free

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i &lt; argc; i++)
+{
+printf(&quot;%s\n&quot;, argv[i]);
+}
+return (0);
+}
