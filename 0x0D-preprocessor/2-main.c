71b65f5c6fea5f9fbce919c95607956bdf831565
Nathaniel <nathaniellesiba@yahoo.com>
2023-04-17 15:32:16 -0700

    FILE NAME

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+/*#include <stdlib>*/
+#include <stdio.h>
+/**
+*main - printing name of the file
+*allowed to use std library
+*Return: zero 0 (success)
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
