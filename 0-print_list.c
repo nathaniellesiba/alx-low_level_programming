7f8e9a871405602b6039799c781caf5f10c76ab8
Nathaniel <nathaniellesiba@yahoo.com>
2023-04-28 18:14:19 -0700

    task0

--- 0-print_list.c
+++ 0-print_list.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * print_list - printing all elements
@@ -8,7 +9,8 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0
+size_t count = 0;
+
 while (h != NULL)
 {
 if (h->str == NULL)
--- lists.h
+++ lists.h
@@ -1,5 +1,6 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,3 +23,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 char _putchar(char c);
+
+#endif
