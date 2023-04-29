df8ff4c4ea152416f9d979e2327c15a2421462ab
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-04-29 04:01:53 +0200

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+void print_message(void)
+{
+fprintf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
+}
+
+int main(void)
+{
+print_message();
+/* rest of the code for main function*/
+return (0);
+}
