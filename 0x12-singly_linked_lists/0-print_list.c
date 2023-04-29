879fe3caa8fc9f2b99faa665e0b8403b7e712338
Nathaniel <nathaniellesiba@yahoo.com>
2023-04-28 19:33:58 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,12 @@ size_t count = 0;
 
 while (h != NULL)
 {
-if (h->str == NULL)
+if (h->str != NULL)
 printf("[0] (nil)\n");
 else
 printf("%s\n", h->str);
 count++;
+h = h->next;
 }
 return (count);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "lists.h"
 
+void message(void)__attribute__((constructor));
+
+/**
+ * print_message - prints the sentence
+ */
+
 void print_message(void)
 {
-fprintf("You're beat! and yet, you must allow,
-\nI bore my house upon my back!\n");
-}
-
-int main(void)
-{
-print_message();
-/* rest of the code for main function*/
-return (0);
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
