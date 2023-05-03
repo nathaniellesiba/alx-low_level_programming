7715cfa5d6a2fb57f3bdc477812a6a1de4578c4c
Nathaniel <nathaniellesiba@yahoo.com>
2023-05-03 16:31:16 -0700

    updating

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,32 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
+* main -check the code
 * struct listint_s - singly linked list
-* @n: integer
+* print_listint - print list
 * @next: points to the next node
-* 
+*
 * Return: count of nodes
 * Description: singly linked list node structure
-* 
+*
+* typedef struct listint
+* {
+* int n;
+* struct listint_s *next;
+* }
+* listint_t;
 */
- 
-typedef struct listint_s
-{
-int n;
-struct listint_s *next;	
-}
-listint_t;
 
 int main(void)
 {
-size_t count = 0;
+size_t print_listint (const listint_t *h);
+size_t listint_len(const listint_t *h);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+count = 0
+size_t print_listint(const listint_t *current)
+{
 const listint_t *current = h;
-while (current != NULL){
+while (current != NULL)
+{
 printf("%d\n", current->n);
 current = current->next;
 count++;
+}
 }
 return (0);
 }
