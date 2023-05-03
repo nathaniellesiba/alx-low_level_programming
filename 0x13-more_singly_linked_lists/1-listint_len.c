ec5bba57878eb64bb2540bba873972953dac8694
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-04 00:45:39 +0200

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+* main -check code
+*
+* @h: pointer to the head
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Return: number of elements
+* Description: singly linked list node structure
+*
+*/
+
+int main(void)
+{
+size_t count = 0;
+const listint_t *current = h;
+
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+return count;
+}
