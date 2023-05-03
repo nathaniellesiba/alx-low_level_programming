f4abcb77c5c4304cb48c2a9a8060c2a8ce18375b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-04 00:36:09 +0200

    print all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
 * main - check code
@@ -18,17 +19,18 @@ typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-}
-listint_t;
+
+} listint_t;
 
 int main(void)
 {
 size_t count = 0;
-const listint_t *current = h;
-while (current != NULL){
-printf("%d\n", current->n);
-current = current->next;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
 count++;
 }
-return (0);
+return count;
 }
