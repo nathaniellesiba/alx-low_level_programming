8c80c624fcb162c9bd78479e73eb3dcc49b26ceb
Nathaniel <nathaniellesiba@yahoo.com>
2023-05-04 06:00:39 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,17 +8,7 @@
  * Return: number of nodes
  */
 
-<<<<<<< HEAD
 size_t print_listint(const listint_t *h)
-=======
-typedef struct listint_s
-{
-int n;
-struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
->>>>>>> 229b2951daa91a6a7f34cd042d3d1329386e6806
 {
 size_t num_nodes = 0;
 
