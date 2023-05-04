e425e76b093ff84da825638c107982147c214f8c
Nathaniel <nathaniellesiba@yahoo.com>
2023-05-04 06:38:20 -0700

    Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,30 +5,19 @@
 #include <stddef.h>
 
 /**
-* add_nodeint - add a new node
-*
-* @head: pointer to the head
-* struct listint_s - singly linked list
-* @n: integer
-* @next: points to the next node
-*
-* Return: number of elements
-* Description: singly linked list node structure
-*
+ * add_nodeint - add a new node
+ * @head: header
+ * @n: data
+ * Return: address of the new element, or NULL
 */
 
-typedef struct listint_s
-{
-int n;
-struct listint_s *next;
-} listint_t;
-
-listint_t *add_nodeint(listint_t **head, int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new_node;
 
 new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
+
+if (!new_node)
 return (NULL);
 
 new_node->n = n;
