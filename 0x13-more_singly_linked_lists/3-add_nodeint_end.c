764896ea9dc5f6c5327a43a4b706670417003ad5
Nathaniel <nathaniellesiba@yahoo.com>
2023-05-04 07:45:26 -0700

    Add node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,43 +1,37 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stddef.h>
 
 /**
-* add_nodeint_end - add new node at the end
-*
-* @head: pointer to the head
-* struct listint_s - singly linked list
-* @n: integer
-* @next: points to the next node
-*
-* Return: address of elements
-* Description: singly linked list node structure
-*
+* add_nodeint_end - adds a new node at the end of a listint_t list
+* @head: pointer
+* @n: data to insert
+* Return: address of the new element, or NULL if it failed
 */
 
-typedef struct listint_s
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-int n;
-struct listint_s *next;
-} listint_t;
+listint_t *new_node, *temp;
 
-listint_t *add_nodeint_end(listint_t **head, const int n) {
-listint_t *new_node = malloc(sizeof(listint_t));
-if (new_node == NULL) {
-return NULL;
-}
+new_node = malloc(sizeof(listint_t));
+if (!new_node)
+return (NULL);
+
 new_node->n = n;
 new_node->next = NULL;
-if (*head == NULL) {
+
+if (*head == NULL)
+{
 *head = new_node;
-} else {
-listint_t *temp = *head;
-while (temp->next != NULL) {
+}
+else
+{
+temp = *head;
+while (temp->next != NULL)
+{
 temp = temp->next;
 }
 temp->next = new_node;
 }
-return new_node;
+return (new_node);
 }
