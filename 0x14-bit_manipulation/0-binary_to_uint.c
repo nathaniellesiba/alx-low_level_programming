cdc2f3c9f21c6b017bb914df7df8b2e900b35903
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-07 01:33:16 +0200

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* converts a binary number to an unsigned int
+* @b: points to string 0 and 1 chars
+*
+* return: the converted number or 0
+*
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int res = 0;
+unsigned int base = 1;
+int len = 0;
+if (!b)
+return (0);
+
+while (b[len])
+len++;
+for (int i = len - 1; i >= 0; i--)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+if (b[i] == '1')
+res += base;
+base *= 2;
+}
+return (res);
+}
