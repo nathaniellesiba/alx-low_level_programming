0c9c2280f92d2bcca12e0ceb3226d5b45e2b85e7
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-07 01:48:34 +0200

    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main) $ ls README.md
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ vi main.h
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ ls
    README.md  main.h
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ vi 0-binary_to_uint.c
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ git add .
    warning: in the working copy of '0x14-bit_manipulation/0-binary_to_uint.c', LF will be replaced by CRLF the next time Git tou
    ches it
    warning: in the working copy of '0x14-bit_manipulation/README.md', LF will be replaced by CRLF the next time Git touches it
    warning: in the working copy of '0x14-bit_manipulation/main.h', LF will be replaced by CRLF the next time Git touches it
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ git commit -m 0-binary_to_uint.c
    [main cdc2f3c] 0-binary_to_uint.c
     3 files changed, 54 insertions(+)
     create mode 100644 0x14-bit_manipulation/0-binary_to_uint.c
     create mode 100644 0x14-bit_manipulation/README.md
     create mode 100644 0x14-bit_manipulation/main.h
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ git push
    Enumerating objects: 7, done.
    Counting objects: 100% (7/7), done.
    Delta compression using up to 20 threads
    Compressing objects: 100% (5/5), done.
    Writing objects: 100% (6/6), 934 bytes | 467.00 KiB/s, done.
    Total 6 (delta 1), reused 0 (delta 0), pack-reused 0
    remote: Resolving deltas: 100% (1/1), completed with 1 local object.
    To https://github.com/nathaniellesiba/alx-low_level_programming.git
       1ae5252..cdc2f3c  main -> main
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ vi 0-binary_to_uint.c
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ git add .
    warning: in the working copy of '0x14-bit_manipulation/0-binary_to_uint.c', LF will be replaced by CRLF the next time Git tou
    ches it
    
    A217355@LSBSA-JQDTVL3 MINGW64 ~/alx-low_level_programming/0x14-bit_manipulation (main)
    $ git commit -mq

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,21 +13,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int res = 0;
-unsigned int base = 1;
-int len = 0;
-if (!b)
+unsigned int num = 0;
+int len, i;
+
+if (b == NULL)
 return (0);
 
-while (b[len])
-len++;
-for (int i = len - 1; i >= 0; i--)
+len = strlen(b);
+
+for (i = 0; i < len; i++)
 {
 if (b[i] != '0' && b[i] != '1')
 return (0);
+
 if (b[i] == '1')
-res += base;
-base *= 2;
+num += pow(2, len - i - 1);
 }
-return (res);
+
+return (num);
 }
