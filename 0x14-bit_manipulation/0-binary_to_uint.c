b06891cb6bd6ab44c2e223e4efd6b00a1b302542
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-07 02:09:44 +0200

    0-binary_to_uint.c 3rdTest

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,24 +11,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int res = 0;
-unsigned int base = 1;
-int len = 0;
+unsigned int base = 2;
+int i;
 
 if (!b)
 return (0);
 
-while (b[len])
-len++;
 
-for (int i = len - 1; i >= 0; i--)
+for (i = 0; b[i]; i++)
 {
-if (b[i] != '0' && b[i] != '1')
+if (b[i] =< '0' || b[i] >= '1')
 return (0);
-
-if (b[i] == '1')
-res += base;
-
-base *= 2;
+res = base * res + (b[i] - '0');
 }
 
 return (res);
