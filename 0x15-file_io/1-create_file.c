28105bc358ebb3bd3c85adbb82757021fe372f8b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-09 01:02:18 +0200

    1-create_file2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
 * create_file - This function creates a file
-* with the given filename and writes the given text content to it.
-* If the file already exists
-* it will not change the permissions and will truncate the file.
-* If the filename or text_content is NULL, it will return -1.
-* The created file will have permissions rw-------.
+*
 * @text_content: content
 * Parameters:
 * filename (const char*): The name of the file to create
@@ -21,23 +13,26 @@
 */
 
 int create_file(const char *filename, char *text_content)
+
 {
 if (filename == NULL)
 {
 return (-1);
 }
+
 int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 {
 return (-1);
 }
+
+
 if (text_content != NULL)
 {
 int len = strlen(text_content);
 int bytes_written = write(fd, text_content, len);
 if (bytes_written != len)
 {
-close(fd);
 return (-1);
 }
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
