38a2a335fc6e74ac3f529d7a7ecf9122560fa305
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-09 00:26:49 +0200

    Modified

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,26 +12,22 @@
 * Return: printed numbers and letters
 */
 
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 int fd;
 ssize_t bytes_read, bytes_written;
 char *buf;
 
-if (!filename)
-return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
+
 buf = malloc(sizeof(char) * letters);
-if (!buf)
-{
-close(fd);
-return (0);
-}
 bytes_read = read(fd, buf, letters);
-if (bytes_read == -1)
-{
+bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+
+
 free(buf);
 close(fd);
-return (0);
+return (bytes_written);
 }
