6b471247a7530a734087d4d17e6577294e0f3549
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-11 21:44:32 +0200

    second task04

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,12 +16,6 @@ int create_file(const char *filename, char *text_content)
 int fd, bytes_written, len = 0;
 
 if (filename == NULL)
-{
-return (-1);
-}
-
-fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-if (fd == -1 || bytes_written == -1)
 return (-1);
 
 if (text_content != NULL)
@@ -29,6 +23,10 @@ if (text_content != NULL)
 for(len = strlen(text_content[len]); 0;)
 len++;
 }
+
+fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+if (fd == -1 || bytes_written == -1)
+return (-1);
 
 close(fd);
 return (1);
