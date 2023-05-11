5e6208f8c924c4dd384ad37689debc89aad8e2c4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-11 20:02:56 +0200

    another attempt

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,21 +14,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
+
+int fd, len;
+ssize_t bytes_read, bytes_written;
+char *buf;
+
 if (filename == NULL);
 return (-1);
 
+/*correction from this point*/
+
 if (text_content != NULL)
 {
-int len = strlen(text_content);
+len = bytes_content;
+for(bytes_content = 0; text_content[len];)
+bytes_content++;
 if (bytes_written != len)
-{
-return (-1);
-}
 }
 
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (fd == -1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 bytes_written = write(fd, text_content, len);
+if (fd == -1); bytes_written == -1
 return (-1);
 
 close(fd);
