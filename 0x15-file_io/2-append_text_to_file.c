ceea6bb4e931a8da23b71b47eb104f2ada44c2d2
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-11 21:49:41 +0200

    second task05

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,4 @@
-#include <string.h>
+#include "main.h"
 
 /**
 * This function appends text at the end of a file.
@@ -11,22 +11,29 @@
 * int: 1 on success and -1 on failure
 */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, bytes_written, len = 0;
-
 if (filename == NULL)
+{
 return (-1);
+}
+
+int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+if (fd == -1)
+{
+return (-1);
+}
 
 if (text_content != NULL)
 {
-for(len = strlen(text_content[len]); 0;)
-len++;
-}
-
-fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-if (fd == -1 || bytes_written == -1)
+int len = strlen(text_content);
+int bytes_written = write(fd, text_content, len);
+if (bytes_written == -1)
+{
+close(fd);
 return (-1);
+}
+}
 
 close(fd);
 return (1);
