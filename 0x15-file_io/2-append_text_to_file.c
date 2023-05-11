498629d8d8b8e4a88ac57b53cd03feb5ac6cb666
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-11 21:24:25 +0200

    second task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,29 +1,26 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include "main.h"
 
 /**
-*This function appends text at the end of a file.
+* This function appends text at the end of a file.
 *
-*Parameters:
-*filename (const char*): The name of the file to append to
-*text_content (char*): The NULL terminated string to add at the end of the file
+* Parameters:
+* filename (const char*): The name of the file to append to
+* text_content (char*): The NULL terminated string to add at the end of the file
 *
-*Returns:
-*int: 1 on success and -1 on failure
+* Returns:
+* int: 1 on success and -1 on failure
 */
 
 int create_file(const char *filename, char *text_content)
 {
+int fd, bytes_written = 0;
+
 if (filename == NULL)
 {
 return (-1);
-}i
+}
 
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
 if (fd == -1)
 {
 return (-1);
@@ -32,13 +29,14 @@ return (-1);
 if (text_content != NULL)
 {
 size_t len = strlen(text_content);
-ssize_t bytes_written = write(fd, text_content, len);
-if (bytes_written == -1 || (size_t) bytes_written != len)
+bytes_written = write(fd, text_content, len);
+if (bytes_written == -1 || (size_t)bytes_written != len)
 {
 close(fd);
 return (-1);
 }
 }
+
 close(fd);
-return 1;
+return (1);
 }
