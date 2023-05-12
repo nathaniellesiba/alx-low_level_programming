e0421df68f66c3adcfb0138c67c0878e7f819141
Nathaniel <nathaniellesiba@yahoo.com>
2023-05-12 12:08:14 -0700

    hello world

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
-* This function appends text at the end of a file.
-*
-* Parameters:
-* filename (const char*): The name of the file to append to
-* text_content (char*): The NULL terminated string to add at the end of the file
-*
-* Returns:
-* int: 1 on success and -1 on failure
+* Description: 'append text to file'
+* Parameters: describe the parameter
+* filename: name of the file to append
+* text_content: contents of file
+* fd: file action
+* terminated string to add
+* append_text_to_file - appending
+* Returns: always (1) on success
 */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-
+int fd, bytes_written, len = 0;
 if (filename == NULL)
 return (-1);
 
 
 if (text_content != NULL)
 {
-int len = (strlen(text_content); 0;)
+len = strlen(text_content) || 0;
 len++;
 }
 
-int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-int bytes_written = write(fd, text_content, len);
+fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+bytes_written = write(fd, text_content, len);
 if (fd == -1 || bytes_written == -1)
 return (-1);
 
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,7 +43,8 @@ while ((ret_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 ret_write = write(fd_to, buffer, ret_read);
 if (ret_write == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
@@ -51,7 +52,8 @@ exit(99);
 
 if (ret_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
@@ -59,7 +61,8 @@ exit(98);
 ret_close = close(fd_from);
 if (ret_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+dprintf(STDERR_FILENO
+"Error: Can't close fd %d\n", fd_from);
 exit(100);
 }
 
@@ -67,7 +70,8 @@ exit(100);
 ret_close = close(fd_to);
 if (ret_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+dprintf(STDERR_FILENO,
+"Error: Can't close fd %d\n", fd_to);
 exit(100);
 }
 
