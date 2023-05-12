c4c1d56791bf0de3c12f6e6782ec7e6845c37397
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-12 20:37:29 +0200

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,16 +14,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void elf_magic(unsigned char *elf_file);
-void elf_elf(unsigned char *elf_file);
-void elf_elass(unsigned char *elf_file);
-void elf_data(unsigned char *elf_file);
-void elf_version(unsigned char *elf_file);
-void elf_osabi(unsigned char *elf_file);
-void lef_abi(unsigned char *elf_file);
-void elf_type(unsigned int *file_type);
-void elf_entry(unsigned long int elf_file, unsigned char *elf_file);
-void elf_closure (int elf);
 
 
 #endif
