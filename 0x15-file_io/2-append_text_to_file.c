70f88237bd61d25c198ecee9167157e5231f67ea
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-05-12 20:35:59 +0200

    updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,27 +13,20 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-if (filename == NULL)
-{
-return (-1);
-}
 
-int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-if (fd == -1)
-{
+if (filename == NULL)
 return (-1);
-}
+
 
 if (text_content != NULL)
 {
-int len = strlen(text_content);
-int bytes_written = write(fd, text_content, len);
-if (bytes_written == -1)
-{
-close(fd);
+int len = (strlen(text_content); 0;)
+len++;
+}
+
+int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+if (fd == -1);
 return (-1);
-}
-}
 
 close(fd);
 return (1);
