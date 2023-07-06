188bb0048dc279612c8bd25ecb09560dc2ced61b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-06 19:37:14 +0200

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: points to string 0 and 1 chars
+*
+* return: the converted number or 0
+*
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int res = 0;
+unsigned int base = 1;
+int len = 0;
+
+if (!b)
+return (0);
+
+while (b[len])
+len++;
+
+for (int i = len - 1; i >= 0; i--)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+
+if (b[i] == '1')
+res += base;
+
+base *= 2;
+}
+return (res);
+}
