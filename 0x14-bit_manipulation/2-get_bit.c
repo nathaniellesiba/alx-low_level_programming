6213ddbc88087f670eae4b1259c085bbc269f45e
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-06 19:45:46 +0200

    submission 01

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - value of a bit at a given index
+ * @index: starts from 0 of bit
+ *
+ * @n: integer
+ * return: value of bit at index
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+int index_bit;
+if (index > (sizeof(unsigned long int) * 8))
+return (-1);
+index_bit = (n >> index) & 1;
+return (index_bit);
+}
