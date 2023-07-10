ec2ef53182246dfdf46a306e26824a46304f501a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-11 00:12:47 +0200

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,6 +30,7 @@ return (BUFFER);
 void closure (int fd);
 {
 int t;
+t = close(fd);
 if(t == -1)
 {
 dprintf(STDERR_FILENO,
@@ -38,3 +39,71 @@ exit(100);
 }
 }
 
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to, ret_read, ret_write, ret_close;
+char buffer[BUFFER];
+
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+
+
+while ((ret_read = read(fd_from, buffer, BUFFER)) > 0)
+{
+ret_write = write(fd_to, buffer, ret_read);
+if (ret_write == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
+
+
+if (ret_read == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+
+ret_close = close(fd_from);
+if (ret_close == -1)
+{
+dprintf(STDERR_FILENO
+"Error: Can't close fd %d\n", fd_from);
+exit(100);
+}
+
+
+ret_close = close(fd_to);
+if (ret_close == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't close fd %d\n", fd_to);
+exit(100);
+}
+
+return (0);
+}
