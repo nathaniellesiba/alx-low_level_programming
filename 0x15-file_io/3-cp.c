8876894232b780d3a558672923ba3066dbd24fa2
nathaniellesiba <nathaniellesiba@yahoo.com>
2023-07-13 11:05:30 -0700

    phew

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,55 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER 1024
+#define BUFFER_SIZE 1024
+
+char *buffer_file(char *file);
+void closure(int fd);
 
 /**
-* main - copies the content of a file to another file
-* @argc: number of arguments passed to the program
-* @file: the buffer size file
-* @fd: file desc not closed
-* @argv: array of arguments passed to the program
-*
-* Return: 0 on success, or an error code on failure
+* buffer_file - Allocates BUFFER_SIZE bytes for a buffer.
+* @file: name of file
+* Return: pointer to new buffer
 */
 
-char buffer_file (char *file)
+char *buffer_file(char *file)
 {
-if(BUFFER == NULL)
+char *buff;
+
+buff = malloc(sizeof(char) * BUFFER_SIZE);
+
+if (buff == NULL)
 {
 dprintf(STDERR_FILENO,
-"Error: Can't write to %s/n",file);
+"Error: Can't write to %s\n", file);
 exit(99);
 }
-return (BUFFER);
+
+return (buff);
 }
 
+/**
+ * closure - closes file
+ * @fd: file to be closed
+ */
 
-
-void closure (int fd);
+void closure(int fd)
 {
 int t;
+
 t = close(fd);
-if(t == -1)
+
+if (t == -1)
 {
 dprintf(STDERR_FILENO,
-"Error: Can't close fd %d/n", fd);
+"Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
 
+/**
+ * main -copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers
+ * Return: 0 on success
+ */
+
 int main(int argc, char *argv[])
 {
 int fd_from, fd_to, ret_read, ret_write, ret_close;
-char buffer[BUFFER];
-
+char buffer[BUFFER_SIZE];
 
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-
 
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
@@ -59,7 +73,6 @@ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
-
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 {
@@ -67,8 +80,7 @@ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 
-
-while ((ret_read = read(fd_from, buffer, BUFFER)) > 0)
+while ((ret_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 ret_write = write(fd_to, buffer, ret_read);
 if (ret_write == -1)
@@ -79,7 +91,6 @@ exit(99);
 }
 }
 
-
 if (ret_read == -1)
 {
 dprintf(STDERR_FILENO,
@@ -87,21 +98,18 @@ dprintf(STDERR_FILENO,
 exit(98);
 }
 
-
 ret_close = close(fd_from);
 if (ret_close == -1)
 {
-dprintf(STDERR_FILENO
+dprintf(STDERR_FILENO,
 "Error: Can't close fd %d\n", fd_from);
 exit(100);
 }
 
-
 ret_close = close(fd_to);
 if (ret_close == -1)
 {
-dprintf(STDERR_FILENO,
-"Error: Can't close fd %d\n", fd_to);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 exit(100);
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,23 +12,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-<<<<<<< HEAD
-
-char buffer_file (char *file);
-void closure (int fd);
-int main(int argc, char* argv[]);
-void display_error(const char* message);
-void display_elf_header(const char* filename);
-void check(unsigned char *elf_pointer);
-void magic(unsigned char *elf_pointer);
-
-
-char buffer_file(char *file);
-void closure(int fd);
-int main(int argc, char *argv[]);
-void display_error(const char *message);
-
-
-=======
->>>>>>> 99291976b3ec99d48305a138a7a93b1cde9c93f9
 #endif
