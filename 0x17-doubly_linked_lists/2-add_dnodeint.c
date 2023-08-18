f8a567b73c65b4fb6f16c8b7f7c99057d167b4e8
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-19 00:03:27 +0200

    task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,24 +12,20 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new = malloc(sizeof(dlistint_t));
-dlistint_t *h = *head;
 
 if (new == NULL)
+{
 return (NULL);
+}
 
 new->n = n;
 new->prev = NULL;
+new->next = *head;
 
-if (h != NULL)
+if (*head != NULL)
 {
-while (h->prev != NULL)
-h = h->prev;
+(*head->prev = new);
 }
-
-new->next = h;
-
-if (h != NULL)
-h->prev = new;
 
 *head = new;
 
