4f8b2e165b867214b5b0c89c2d0130b68d43215d
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-18 23:53:56 +0200

    test 3 of 1

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,4 @@
-i#include "lists.h"
+#include "lists.h"
 
 /**
 * add_dnodeint_end - adds new node
@@ -11,11 +11,8 @@ i#include "lists.h"
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node,
-	temp;
-
 dlistint_t *new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
+if (new_node == NULL) 
 {
 return (NULL);
 }
@@ -27,9 +24,10 @@ if (*head == NULL)
 {
 new_node->prev = NULL;
 *head = new_node;
-return (new_node);
 }
-
+else
+{
+dlistint_t *temp = *head;
 while (temp->next != NULL)
 {
 temp = temp->next;
@@ -37,6 +35,5 @@ temp = temp->next;
 temp->next = new_node;
 new_node->prev = temp;
 }
-
 return (new_node);
 }
