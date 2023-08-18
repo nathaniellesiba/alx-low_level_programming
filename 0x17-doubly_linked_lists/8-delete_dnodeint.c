dc80944908674da6bdefbe0bd84f99cc3fd38683
nathaniel <nathaniellesiba@yahoo.com>
2023-08-18 16:55:15 -0700

    task8 betty

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,12 +5,12 @@
 * the node at index of a dlistint_t
 * linked list
 * @head: pointer to the header
-* @index: node that should be deleted.
+* @index: node that should be deleted
 * Index starts at 0
 * Return: 1 on success or -1 on failure
 */
 
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) 
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *new, *new_node;
 unsigned int x;
