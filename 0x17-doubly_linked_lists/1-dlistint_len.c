3dfe38afeadfd4533d28cf090776a2b709f55192
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-18 23:14:15 +0200

    task1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stddef.h>
 
 /**
 * print_dlistint - print elements of
@@ -7,21 +8,15 @@
 * Return: count of nodes
 */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-int count = 0;
+    size_t count = 0;
 
-if (h == NULL)
-return(count);
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
 
-while (h != NULL)
-{
-count++;
-h = h->next;
-}
-
-while (h->prev !=NULL)
-h = h->prev;
-
-return (count);
+    return count;
 }
