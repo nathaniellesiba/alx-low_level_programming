b14b3d0e745f6487e465e18d83323d35569f660b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-19 00:24:08 +0200

    whitespaces fix 1 and 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 
 /**
-* add_dnodeint - adds a new node at the beginning
+* add_dnodeint - adds a new node
+* at the beginning
 * of a dlistint_t list
 *
 * @head: head of the list
 * @n: value of the element
-* Return: the address of the new element
+* Return: address of the new element
 */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL) 
+if (new_node == NULL)
 {
 return (NULL);
 }
